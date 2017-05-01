93190b3c9d969a556518e07d59072d1d7065e031
Chris <chriskim80@gmail.com>
2017-05-01 15:51:09 -0400

    Finished ArrayRotator

--- src/main/java/rotateArray/ArrayRotator.java
+++ src/main/java/rotateArray/ArrayRotator.java
@@ -1,11 +1,34 @@
 package rotateArray;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class ArrayRotator extends ArrayList{
+public class ArrayRotator<E> extends ArrayList{
 
-    public static void main(String[] args){
-        
+
+
+    public void rotateArray(int numOfPositions){
+        for(int i = 0; i < numOfPositions; i++){
+            this.add(this.get(0));
+            this.remove(0);
+        }
     }
 
+    public static void main(String[] args){
+        ArrayRotator<String> test = new ArrayRotator<String>();
+
+        System.out.println("Original List: ");
+        for(int i = 0; i < 10; i ++){
+            System.out.print("[" + i + "], ");
+            test.add(Integer.toString(i));
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("\n\nEnter number of positions to rotate: ");
+        test.rotateArray(input.nextInt());
+
+        for(int j = 0; j < test.size(); j++){
+            System.out.print("[" + test.get(j) + "], ");
+        }
+    }
 }
