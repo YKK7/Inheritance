d3a4ab97575a8b40b4bacdf8bf60c534972045c0
Chris <chriskim80@gmail.com>
2017-05-03 09:23:11 -0400

    Wrote basic methods and made Account abstract

--- src/main/java/abstractClass/Account.java
+++ src/main/java/abstractClass/Account.java
@@ -1,12 +1,63 @@
 package abstractClass;
 
-public class Account {
+public abstract class Account {
 
     private double balance;
     private double interestRate;
-    private int accountNumber;
+    private final int accountNumber;
     private String name;
     private int pinNumber;
+
+    public Account(double balance, double interestRate, int accountNumber, String name, int pinNumber) {
+        this.balance = balance;
+        this.interestRate = interestRate;
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.pinNumber = pinNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPinNumber() {
+        return pinNumber;
+    }
+
+    public void setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public double deposit(double amount){
+        balance += amount;
+        return balance;
+    }
+
+    public abstract double withdraw(double amount);
 
 
 }
