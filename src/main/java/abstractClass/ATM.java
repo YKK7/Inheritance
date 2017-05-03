4fe4eb435e8c66b561c86c5077a54dad6e6fa4b2
Chris <chriskim80@gmail.com>
2017-05-03 12:50:45 -0400

    added test for login

--- src/main/java/abstractClass/ATM.java
+++ src/main/java/abstractClass/ATM.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 public class ATM{
 
     private ArrayList<Account> accountsList;
+    private boolean verified;
 
     public ATM(ArrayList<Account> accountsList) {
         this.accountsList = accountsList;
+        verified = false;
     }
 
     public ATM(){
         accountsList = new ArrayList<Account>();
+        verified = false;
     }
 
     public ArrayList<Account> getAccountsList() {
@@ -35,5 +38,33 @@ public class ATM{
         }
     }
 
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public boolean login(int accountNumber, int pin){
+        
+        for(int i = 0; i < accountsList.size(); i++){
+            if(accountsList.get(i).getAccountNumber() == accountNumber && accountsList.get(i).getPinNumber() == pin) {
+
+                this.verified = true;
+            }
+        }
+        return verified;
+
+    }
+
+    public void displayMenu(){
+
+
+    }
+
+    public double withdraw(int acctNumber, double amount){
+        return 0;
+    }
+
+    public double deposit(int acctNumber, double amount){
+        return 0;
+    }
 
 }
